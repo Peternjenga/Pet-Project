289fbcc39dadb12ab06162fdf1b25c7811fd0dab
Peternjenga <pnjengah@gmail.com>
2021-07-20 15:09:11 +0300

    button to change background color using jQuery

---
+++ js/scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+    $("button#black").click(function () {
+        $("body").removeClass();
+        $("body").addClass("black-background")
+    });
+});
+
+    $(document).ready(function () {
+        $("button#white").click(function () {
+            $("body").removeClass();
+            $("body").addClass("white-background")
+        })
+
+});
